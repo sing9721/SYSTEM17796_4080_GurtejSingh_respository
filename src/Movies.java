3da6959078fb42c1b5ee396ebfc2445068e90f91
sing9721 <sing9721@sheridancollege.ca>
2020-01-22 19:12:18 -0500

    initial commit for new Student code

--- src/Movies.java
+++ src/Movies.java
@@ -1,12 +1,29 @@
 
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class Movies
-{
-	public static void main (String args[ ])
-	{
-		String Names[ ] ={"pursuit", "fast five", "king kong", "king", "heman"};
-		String Name = Arrays.toString(Names);
-		          System.out.println(Name);
-        }
+        {
+
+   
+    private String StudentDemo;
+
+    public String getStudentDemo() {
+        return StudentDemo;
+    }
+
+    public void setStudentDemo(String _StudentDemo) {
+        StudentDemo = _StudentDemo;
+    }
+ 
+      
 }
+/**{
+  
+
+	//public static void main (String args[ ])
+	{
+		//String Names[ ] ={"pursuit", "fast five", "king kong", "king", "heman"};
+              //                  String Name = Arrays.toString(Names);
+		// System.out.println(Name);
+        }
+}**/
